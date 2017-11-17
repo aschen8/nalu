9d7b9e378b36b9cb78f7e01419ba1490921b06db
Omar Esai Vazquez <ovazquez@eng.ucsd.edu>
2017-11-16 19:46:59 -0800

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -14,33 +14,3 @@ CREATE TABLE products(
     PRIMARY KEY(item_id)
 );
 
--- Insert data into the 'products' table --
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("T-shirt/Shorts/Pants", "Womens Clothing", 24.99, 10);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("T-shirt/Shorts/Pants", "Mens Clothing", 24.99, 15);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("T-shirts/Shorts/Pants", "Childrens Clothing", 39.99, 20);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("Shoes", "Womens Shoes", 99.00, 5);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("Shoes", "Mens Shoes", 89.00, 25);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("Shoes", "Children Shoes", 25.00, 2);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("Jackets", "Outdoor Clothing", 100.00, 7);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("Socks", "Under Garments", 10.00, 12);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("Hair Spray", "Hair Products", 23.99, 5);
-
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES("Green Eggs & Ham", "Books", 21.00, 1);
