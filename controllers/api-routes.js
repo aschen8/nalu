e84217c7e8dcd0f53183b773873016aaa5c7795f
Omar Esai Vazquez <ovazquez@eng.ucsd.edu>
2017-11-18 14:16:03 -0800

    Add files via upload

---
+++ controllers/api-routes.js
@@ -0,0 +1,27 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+// Dependencies
+// =============================================================
+var connection = require("../config/businessManager.js");
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Get all chirps
+  app.get("/api/all", function(req, res) {
+    var dbQuery = "SELECT * FROM business";
+    connection.query(dbQuery, function(err, result) {
+      res.json(result);
+    });
+  });
+
+  app.post("/api/new", function(req, res) {
+    console.log("Business Data:");
+    console.log(req.body);
+    var dbQuery = "INSERT INTO business (ItemID, product_name, sku, category_name, price, stock_quantity) VALUES (?,?,?)";
+    connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function(err, result) {
+      console.log("Inventory Successfully Saved!");
+      res.end();
+    });
+  });
+};
---
+++ controllers/business.js
@@ -0,0 +1,45 @@
+/* global moment */
+// When the page loads, grab and display all of our inventory
+$.get("/api/all", function(data) {
+  if (data.length !== 0) {
+    for (var i = 0; i < data.length; i++) {
+      var row = $("<div>");
+      row.addClass("Item");
+      row.append("Item ID: " + data[i].item_id + "  //  ";
+      row.append("Sku: " + data[i].sku + " // ";
+      row.append("Product Name: " + data[i].product_name + "  //  ";
+      row.append("Price: $" + data[i].price + "  //  ";
+      row.append("Category: " + data[i].category_name + "  //  ";
+      row.append("Quantity: " + data[i].stock_quantity + "\n";
+      $("business").prepend(row);
+    }
+  }
+});
+// When manager adds to inventory (clicks addBtn)
+$("#submitButton").on("click", function(event) {
+  event.preventDefault();
+  // Make a newItem object
+  var newItem = {
+    author: $("#author").val().trim(),
+    body: $("#chirp-box").val().trim(),
+    created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+  };
+  console.log(newItem);
+  // Send an AJAX POST-request with jQuery
+  $.post("/api/new", newItem)
+    // On success, run the following code
+    .done(function() {
+      var row = $("<div>");
+      row.addClass("Item");
+      row.append("Sku: " + data[i].sku + " // ";
+      row.append("Product Name: " + data[i].product_name + "  //  ";
+      row.append("Price: $" + data[i].price + "  //  ";
+      row.append("Category: " + data[i].category_name + "  //  ";
+      row.append("Quantity: " + data[i].stock_quantity + "\n";
+      $("business").prepend(row);
+      
+    });
+  // Empty each input box by replacing the value with an empty string
+  $("#author").val("");
+  $("#chirp-box").val("");
+});
