243aec4ad3b63cdc00222ea768eb8e373fb196a7
Chryssa <chryssajones@gmail.com>
2017-11-28 19:17:11 -0800

    added inventory link to index

--- server/controllers/api-routes.js
+++ server/controllers/api-routes.js
@@ -39,20 +39,32 @@ module.exports = function(app) {
 });
 
 
-  app.post("/inventory", function(req, res) {
-    console.log("app.post has been called from /inventory");
-    console.log("Business Data:");
-    console.log(req.body);
-    var dbQuery = "INSERT INTO products (product_name, sku, category, price, stock_quantity, reorder_quantity) VALUES (?,?,?)";
-    connection.query(dbQuery, [req.body], function(err, result) {
-      if (err) throw err;
-      console.log("Inventory Successfully Saved!");
-      res.end();
+app.post("/inventory", function(req, res) {
+        var sku = req.body.sku;
+        var product_name = req.body.product_name;
+        var category = req.body.category;
+        var cost = req.body.cost;
+        var price = req.body.price;
+        var stock_quantity = req.body.stock_quantity;
+        var reorder_quantity = req.body.reorder_quantity;
+
+        var addItemQuery = "INSERT INTO products " + 
+        "(sku, product_name, category, cost, price, stock_quantity, reorder_quantity) " +
+        "VALUES (" + sku + ", '" + product_name + "', '"
+        + category + "', " + cost + ", " + price + ", "
+        + stock_quantity + ", " + reorder_quantity + ")"
+
+        console.log(addItemQuery);
+
+        conn.query(addItemQuery, function(err, result) {
+            if (err) throw err;
+            console.log("New item added.");
+        });
+
+        res.redirect("/inventory");
     });
-  });
 
-};
-
+}
 
 
 // Sequelize Routes
